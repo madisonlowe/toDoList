cfd4edde1b96573a954889bb05b9391fe3d33522
madisonlowe <mcourteney@live.com>
2022-06-16 22:06:14 +0100

    Added handleAdd and map function to map tasks onto ul/li.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,89 +1,52 @@
 import "./App.css";
 import { useState } from "react";
 
-function Input() {
+// we make a component that has an input and a button DONE
+// we set the current value of the input as equal to a variable DONE
+// every time that value updates, we set the state of text and update it DONE
+// the text state is whatever the current text in the input is DONE
+
+const taskArray = [];
+
+function Input({ handleChange, handleAdd, text }) {
+  return (
+    <div>
+      <input onChange={handleChange} value={text}></input>
+      <button onClick={handleAdd}>Add to List</button>
+    </div>
+  );
+}
+
+// now, we want to take that text state
+// on clicking the button in input, the button should call another function (handleAdd)
+// handleAdd should update the state of the tasks array with the new text state data added
+// i declared a list array for the todo list data
+// then, i added a handleAdd function, which resets the state of the task list immutably
+// atm, i don't have a component for rendering the list, i just render it via mapping in the app function
+
+function App() {
+  const [tasks, setTasks] = useState(taskArray);
   const [text, setText] = useState("");
 
   function handleChange(e) {
     setText(e.target.value);
   }
 
-  console.log(text);
+  function handleAdd() {
+    const newTasks = tasks.concat({ todo: text });
+    setTasks(newTasks);
+  }
 
-  return (
-    <div>
-      <input onChange={handleChange} value={text}></input>
-      <button>Add to List</button>
-    </div>
-  );
-}
-// we make a component that has an input and a button DONE
-// we set the current value of the input as equal to a variable DONE
-// every time that value updates, we set the state of text and update it DONE
-// the text state is whatever the current text in the input is DONE
-// we will later take this state of the input to copy it to an li on clicking a button
-
-function App() {
   return (
     <main className="App">
-      <Input />
+      <Input handleChange={handleChange} handleAdd={handleAdd} text={text} />
+      <ul>
+        {tasks.map((item) => (
+          <li>{item.todo}</li>
+        ))}
+      </ul>
     </main>
   );
 }
 
 export default App;
-
-/*
-## EASY VERSION: NO COMPONENTS
-
-### App
-
-_state_
-
-- list of to dos
-
-_behaviour_
-
-- add item to list
-- remove item from list
-- render an Input and List
-
-### Input
-
-_state_
-
-- text
-
-_props_
-
-- add function
-
-_behaviour_
-
-- render an input field and a button ✓
-- change of input text will update state of this component
-- button click will call the add function from props with value of input
-
-### List
-
-_props_
-
-- array of to dos
-- delete function
-
-_behaviour_
-
-- render an array of ListItems in a ul
-
-### List item
-
-_props_
-
-- text
-- delete function
-
-_behaviour_
-
-- render the text and a button
-- when the button is clicked call the delete function to delete item at this index
-*/
